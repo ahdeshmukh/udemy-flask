bd77a3b4c49c2320fbfe17aef67b65adc18e0934
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-06-21 11:05:51 -0400

    removing all template files

--- app.py
+++ app.py
@@ -6,41 +6,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/show-users')
-def show_users():
-    users = [
-        {"first_name": "Roy", "last_name": "Swanson"},
-        {"first_name": "Heather", "last_name": "Lee"},
-        {"first_name": "Curtis", "last_name": "Wallace"},
-        {"first_name": "Jessica", "last_name": "Lopez"},
-    ]
-    db_con = None
-    try:
-        db_con = psycopg2.connect(host='localhost', database='flask_db', user='amit', password='password')
-        cursor = db_con.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
-        cursor.execute("SELECT id, first_name, last_name, email FROM users")
-        users = cursor.fetchall()
-    except psycopg2.DatabaseError as e:
-        print(e)
-        sys.exit(1)
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
-        raise
-    finally:
-        if db_con is not None:
-            db_con.close()
-
-    print('coming here')
-    return render_template('users.html', users=users)
+def index():
+    return "Index page"
 
 
 @app.route('/post-json', methods=["POST"])
