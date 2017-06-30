fdfc9b7f33bbafd3b1db6fa03f40d32280ae55af
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-06-30 13:23:00 -0400

    miscallenous folder - saving sql file used to create tables on heroku

---
+++ miscellaneous/sql/1.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS flask_role;
+CREATE TABLE flask_role(
+	id serial PRIMARY KEY,
+	name VARCHAR (255) UNIQUE NOT NULL
+);
+
+DROP TABLE IF EXISTS flask_user;
+CREATE TABLE flask_user(
+	id serial PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+ 	email VARCHAR (100) UNIQUE NOT NULL,
+ 	password VARCHAR (255) NOT NULL,
+	gender CHAR(1) DEFAULT NULL CHECK (gender IN ('m', 'f')),
+ 	created_on TIMESTAMP NOT NULL DEFAULT NOW(),
+ 	last_login TIMESTAMP
+);
+
+DROP TABLE IF EXISTS flask_user_role;
+CREATE TABLE flask_user_role
+(
+	id serial PRIMARY KEY,
+	user_id INTEGER NOT NULL,
+	role_id INTEGER NOT NULL,
+	grant_date TIMESTAMP NOT NULL DEFAULT NOW(),
+  	UNIQUE (user_id, role_id),
+  	CONSTRAINT flask_user_role_role_id_fkey FOREIGN KEY (role_id) REFERENCES flask_role (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
+  	CONSTRAINT flask_user_role_user_id_fkey FOREIGN KEY (user_id) REFERENCES flask_user (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+
+INSERT INTO flask_role (name) VALUES ('admin');
+INSERT INTO flask_role (name) VALUES ('authenticated');
+
+INSERT INTO flask_user (first_name, last_name, email, password) VALUES ('Admin', 'Admin', 'admin@example.com', '$6$rounds=656000$S4GS5srTzNh9j.5t$R9q0OsFiiQK3KxDv5kKGN8mTgntmZKMbbBOZE5boubXVB2kSOSy0yGEFMT847NDN0sw/0qu0SqWh1QIq8c9um1')
