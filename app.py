4040f30c098b5fbf0f453e32f774fddb94963714
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-08-15 13:57:53 -0400

    code reformat

--- app.py
+++ app.py
@@ -3,14 +3,10 @@
 from flask import Flask
 import logging
 from logging.handlers import RotatingFileHandler
-
-
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config.from_pyfile('config.py')
-
 db = SQLAlchemy(app)
 
 from routes import *
